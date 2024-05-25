7858e535cab669fbf457b117e407ff58a9ff6bcd
ian <robles.ian@proton.me>
2024-05-25 01:15:34 -0300

    #01

---
+++ api/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    with open("static/index.html") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content)
