cac86e78f68b68d79457f9de0f6df51721d6a803
ian <robles.ian@proton.me>
2024-05-25 01:50:55 -0300

    #04

--- api/main.py
+++ api/main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+import os
+from fastapi import FastAPI, HTMLResponse
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
-    with open("../static/index.html") as f:
+    # Obtener la ruta absoluta del directorio 'static'
+    static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../static")
+    # Construir la ruta completa al archivo 'index.html'
+    index_path = os.path.join(static_dir, "index.html")
+    # Abrir y leer el contenido del archivo 'index.html'
+    with open(index_path) as f:
         html_content = f.read()
+    # Devolver el contenido HTML como respuesta
     return HTMLResponse(content=html_content)
